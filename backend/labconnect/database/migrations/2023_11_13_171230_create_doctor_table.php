ecd165efe417315fe19ab76a7ad80323acfa7e2a
richzago97 <rich.czago@gmail.com>
2023-11-13 14:54:10 -0300

    feat: created doctor migration

---
+++ backend/labconnect/database/migrations/2023_11_13_171230_create_doctor_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctor', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->string('speciality');
+            $table->string('crm')->unique();
+            $table->timestamps();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctor');
+    }
+};
