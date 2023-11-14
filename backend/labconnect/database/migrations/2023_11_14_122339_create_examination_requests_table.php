d0be87c1aebf4f5a913e8cff4cf5fb0f65eff726
richzago97 <rich.czago@gmail.com>
2023-11-14 09:28:48 -0300

    feat: created migration examination requests

---
+++ backend/labconnect/database/migrations/2023_11_14_122339_create_examination_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('examination_requests', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id');
+            $table->uuid('doctor_id');
+            $table->timestamps();
+
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('doctor_id')->references('id')->on('doctor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('examination_requests');
+    }
+};
