6a5368b1bc97e7e5527e49a1b9e72cd5d0669bcf
Ricardo Martins <ormartins02@gmail.com>
2023-11-17 17:27:55 -0300

    feat: address requests and relationship with user.

--- backend/labconnect/app/Models/User.php
+++ backend/labconnect/app/Models/User.php
@@ -37,6 +37,10 @@ class User extends Model/** Authenticatable implements JWTSubject*/ {
         'password' => 'hashed',
     ];
 
+    public function address()
+    {
+        return $this->hasOne(Address::class, 'user_id');
+    }
 
     public function getJWTIdentifier() {
         return $this->getKey();
