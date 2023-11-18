2e163bee329256d29bde56e95cf27c99fbdd75ce
davi <daavisouza.santana@gmail.com>
2023-11-17 21:02:34 -0300

    feat: add relationships functions

--- backend/labconnect/app/Models/Laboratory.php
+++ backend/labconnect/app/Models/Laboratory.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Laboratory extends Model
 {
     use HasFactory;
+
+    public function examinationResult() {
+        return $this->hasMany(ExaminationResult::class);
+    }
 }
